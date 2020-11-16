5a262d6b6698b8935edc4dfe91485485ecd6dbc6
Heo Jong Tae <heo9910@naver.com>
2020-11-16 21:17:59 +0900

    .

---
+++ chapter-01-project-configuration/src/test/java/com/study/datajpa/MemberRepositoryTest.java
@@ -0,0 +1,43 @@
+package com.study.datajpa;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.study.datajpa.domain.Member;
+import com.study.datajpa.repository.MemberRepository;
+
+@ActiveProfiles("test")
+@SpringBootTest
+class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    public void test() {
+        Member member = new Member("memberA");
+        memberRepository.save(member);
+        
+        Optional<Member> optional = memberRepository.findById(member.getId());
+        
+        Member findMember = null;
+        if (optional.isPresent()) {
+            findMember = optional.get();
+        }
+        
+        assertNotNull(findMember);
+        assertEquals(findMember.getId(), member.getId());
+        assertEquals(findMember.getUserName(), member.getUserName());
+    }
+
+}
