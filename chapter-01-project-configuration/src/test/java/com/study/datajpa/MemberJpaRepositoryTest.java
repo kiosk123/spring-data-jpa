5378c07387cd1204166673891a0a70594a17eb20
Heo Jong Tae <heo9910@naver.com>
2020-11-16 21:06:03 +0900

    .

--- chapter-01-project-configuration/src/test/java/com/study/datajpa/MemberJpaRepositoryTest.java
+++ chapter-01-project-configuration/src/test/java/com/study/datajpa/MemberJpaRepositoryTest.java
@@ -5,14 +5,20 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.study.datajpa.domain.Member;
 import com.study.datajpa.repository.MemberJpaRepository;
 
-@ExtendWith(SpringExtension.class)
+/**
+ * 테스트시 @SpringBootApplication이 설정된 클래스의 패키와 같은 경로거나
+ * 하위 경로로 패키지 경로를 맞춰줘야 실행됨
+ */
+@ActiveProfiles("test")
 @SpringBootTest
 class MemberJpaRepositoryTest {
     
