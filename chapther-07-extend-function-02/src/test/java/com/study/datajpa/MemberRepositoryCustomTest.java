946ecf9b5c6a2f519ccff7d560d3c5be77013916
Heo Jong Tae <heo9910@naver.com>
2020-11-18 12:22:21 +0900

    .

--- chapther-07-extend-function-02/src/test/java/com/study/datajpa/MemberRepositoryCustomTest.java
+++ chapther-07-extend-function-02/src/test/java/com/study/datajpa/MemberRepositoryCustomTest.java
@@ -8,11 +8,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.study.datajpa.domain.Member;
 import com.study.datajpa.repository.MemberRepository;
 
+@ActiveProfiles("test")
 @SpringBootTest
 @Transactional
 class MemberRepositoryCustomTest {
