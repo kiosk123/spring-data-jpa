1c607cdd3ee188c9fa2b17f5e36431afbc54aed4
Heo Jong Tae <heo9910@naver.com>
2020-11-18 11:46:18 +0900

    .

--- chapther-07-extend-function/src/test/java/com/study/datajpa/MemberRepositoryCustomTest.java
+++ chapther-07-extend-function/src/test/java/com/study/datajpa/MemberRepositoryCustomTest.java
@@ -11,7 +11,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.study.datajpa.domain.Member;
-import com.study.datajpa.domain.Team;
 import com.study.datajpa.repository.MemberRepository;
 
 @SpringBootTest
