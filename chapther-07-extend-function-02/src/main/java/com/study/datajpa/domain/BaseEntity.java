9cbf8530ca01d98886082ee6070797f47878cf8f
Heo Jong Tae <heo9910@naver.com>
2020-11-18 13:09:31 +0900

    .

--- chapther-07-extend-function-02/src/main/java/com/study/datajpa/domain/BaseEntity.java
+++ chapther-07-extend-function-02/src/main/java/com/study/datajpa/domain/BaseEntity.java
@@ -6,7 +6,9 @@ import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 
+import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
@@ -23,4 +25,11 @@ public class BaseEntity {
     
     @LastModifiedDate
     private LocalDateTime updatedDate;
+    
+    @CreatedBy
+    @Column(updatable = false)
+    private String createBy; //등록자
+    
+    @LastModifiedBy
+    private String lastModifiedBy; //수정자
 }
